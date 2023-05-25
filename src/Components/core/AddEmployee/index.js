5af3b9d5b259f621145d40c86e13fd30ebe77734
SARAVANAN <sb9489.rs@gmail.com>
2023-05-26 01:33:13 +0530

    last

--- src/Components/core/AddEmployee/index.js
+++ src/Components/core/AddEmployee/index.js
@@ -1,78 +1,82 @@
-import { useState } from 'react';
 import './addEmployee.css';
-import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const AddEmployee = () => {
     const navigate = useNavigate();
-    const [ employeeDetails, setEmployeeDetails ] = useState({
-        name: "",
-        email: "",
-        address: "",
-        designation: "",
-        mobileNumber: "",
-        bloodgroup: ""
+    const [employeeDetails, setEmployeeDetails] = useState({
+        name: '',
+        email: '',
+        address: '',
+        designation: '',
+        mobileNumber: '',
+        bloodGroup: ''
     })
+
     const handleInput = (value) => {
         return setEmployeeDetails(employee => {
-            return { ...employee, ...value }
+            return {...employee, ...value}
         })
     }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        try {
+        console.log('Submitting employee details...')
+        try{
             const response = await axios.post(`${process.env.REACT_APP_URL}/employees`, employeeDetails);
-            if (response) {
+            if(response){
                 setEmployeeDetails({
-                    name: "",
-                    email: "",
-                    address: "",
-                    designation: "",
-                    mobileNumber: "",
-                    bloodgroup: ""
-                })
+                    name: '',
+                    email: '',
+                    address: '',
+                    designation: '',
+                    mobileNumber: '',
+                    bloodGroup: ''
+                });
                 navigate('/');
             }
-        } catch (error) {
-            console.log("Error: ", error)
+        }catch(error){
+            console.log('Error: ', error)
         }
     }
 
     return (
-        <div className='addEmployee'>
-            <h1>ADD AN EMPLOYEE</h1>
-            <form onSubmit={handleSubmit}>
-                <div className='form-group'>
-                    <label htmlFor='name'>Name</label>
-                    <input id='name' className='form-control' type='text' value={employeeDetails.name} onChange={e => handleInput({ name: e.target.value })} />
+        <div className="addEmployee">
+            <h2>Add an Employee</h2>
+            <form onSubmit={handleSubmit}> 
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input id='name' className="form-control" type='text' value={employeeDetails.name} onChange={e => handleInput({name: e.target.value})} />
                 </div>
-                <div className='form-group'>
-                    <label htmlFor='email'>Email</label>
-                    <input id='email' className='form-control' type='text' value={employeeDetails.email} onChange={e => handleInput({ email: e.target.value })} />
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input id='email' className="form-control" type='text' value={employeeDetails.email} onChange={e => handleInput({email: e.target.value})} />
                 </div>
-                <div className='form-group'>
-                    <label htmlFor='address'>Address</label>
-                    <input id='address' className='form-control' type='text' value={employeeDetails.address} onChange={e => handleInput({ address: e.target.value })} />
+                <div className="form-group">
+                    <label htmlFor="address">Address</label>
+                    <input id='address' className="form-control" type='text' value={employeeDetails.address} onChange={e => handleInput({address: e.target.value})} />
                 </div>
-                <div className='form-group'>
-                    <label htmlFor='designation'>Designation</label>
-                    <input id='designation' className='form-control' type='text' value={employeeDetails.designation} onChange={e => handleInput({ designation: e.target.value })} />
+                <div className="form-group">
+                    <label htmlFor="designation">Designation</label>
+                    <input id='designation' className="form-control" type='text' value={employeeDetails.designation} onChange={e => handleInput({designation: e.target.value})}/>
                 </div>
-                <div className='form-group'>
-                    <label htmlFor='mobileNumber'>Mobile Number</label>
-                    <input id='mobileNumber' className='form-control' type='text' value={employeeDetails.mobileNumber} onChange={e => handleInput({ mobileNumber: e.target.value })} />
+                <div className="form-group">
+                    <label htmlFor="mobileNumber">Mobile Number</label>
+                    <input id='mobileNumber' className="form-control" type='text' value={employeeDetails.mobileNumber} onChange={e => handleInput({mobileNumber: e.target.value})}/>
                 </div>
-                <div className='form-group'>
-                    <label htmlFor='bloodGroup'>Blood Group</label>
-                    <input id='bloodGroup' className='form-control' type='text' value={employeeDetails.bloodGroup} onChange={e => handleInput({ bloodGroup: e.target.value })} />
+                <div className="form-group">
+                    <label htmlFor="bloodGroup">Blood Group</label>
+                    <input id='bloodGroup' className="form-control" type='text' value={employeeDetails.bloodGroup} onChange={e => handleInput({bloodGroup: e.target.value})}/>
                 </div>
+
                 <div className='form-group'>
-                    <input className='btn-btn-primary' type='submit' value="add an employee" />
+                    <input className='btn btn-primary mt-2' type='submit' value='Add an Employee' />
                 </div>
             </form>
-
         </div>
     )
 }
+
 export default AddEmployee;
--- src/Components/core/EmployeesList/index.js
+++ src/Components/core/EmployeesList/index.js
@@ -1,41 +1,68 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const EmployeeList =()=>{
-    return(
+const EmployeeList = () => {
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        getEmployees();
+    }, []);
+
+    const getEmployees = async () => {
+        try{
+            const response = await axios.get(`${process.env.REACT_APP_URL}/employees`);
+            setEmployees(response.data);
+        }catch(error){
+            console.log('Error: ', error);
+        }
+    }
+
+    const handleDelete = async (employeeID) => {
+        try{
+            const response = await axios.delete(`${process.env.REACT_APP_URL}/employees/${employeeID}`);
+            if(response){
+                getEmployees();
+            }
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+
+    return (
         <div>
             <h2>Our Employees</h2>
-<table className="table table-striped">
-    <thead>
-<tr>
-    <th>Name</th>
-    <th>Email</th>
-    <th>Address</th>
-    <th>Designation</th>
-    <th>MobileNumber</th>
-    <th>BloodGroup</th>
-</tr>
-    </thead>
-    <tbody>
-<tr>
-    <td>garun</td>
-    <td>garun</td>
-    <td>garun</td>
-    <td>garun</td>
-    <td>garun</td>
-    <td>garun</td>
-    <td>garun</td>
-    <td>
-        <Link to={`/employee/12345/update`}>Edit</Link>
-        <button className="btn btn-link">Delete</button>
-    </td>
-    
-
-</tr>
-    </tbody>
-</table>
-
-            
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>Designation</th>
+                        <th>Mobile Number</th>
+                        <th>Blood Group</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.length && employees.map((employee, index) => (
+                        <tr key={index}>
+                            <td>{employee.name}</td>
+                            <td>{employee.email}</td>
+                            <td>{employee.address}</td>
+                            <td>{employee.designation}</td>
+                            <td>{employee.mobileNumber}</td>
+                            <td>{employee.bloodGroup}</td>
+                            <td>
+                                <Link  className="btn btn-link"  to={`/employees/${employee._id}/update`}>Edit</Link>
+                                <button className="btn btn-link" onClick={() => handleDelete(employee._id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
+
 export default EmployeeList;
